b595e0974448d50303b625e36dbf98efd81cfb80
NoahSmo <noah.petel.app@gmail.com>
2024-06-04 19:10:15 +0200

    🚧WIP Register

--- src/app/actions/posts/create.ts
+++ src/app/actions/posts/create.ts
@@ -1,14 +1,14 @@
 'use server';
-import { Prisma } from '@/helpers/database';
+import { prisma } from '@/helpers/database';
 
 export default async function CreatePost(form: FormData) {
-        await Prisma.post.create({
+        await prisma.post.create({
             data: {
                 title: form.get('title') as string,
                 text: form.get('text') as string,
                 media: form.get('media') as string,
                 isVisible: (form.get('isVisible') === "true"),
-                userId: form.get('userId') as unknown as number,
+                userId: form.get('userId') as unknown as string,
                 cityId: form.get('cityId') as unknown as number,
             }
         }).then((post) => {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import {Providers} from "@/app/providers";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -17,7 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        {children}
+        <Providers>{children}</Providers>
         </body>
         </html>
     );
